8c9f7732aa6be5342896469fba937aa7a3e3cef0
stephanfalcon <hoobaloob99@gmail.com>
2019-03-01 11:00:21 -0800

    newest version with attack button working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,69 +1,230 @@
+// selectors because I lazy
 var card = $(".card");
-var announcer = $("#announcer")
-// var obiAttack = ;
-// var vaderAttack = ;
-// var sidAttack = ;
-// var lukeAttack= ;
-// var charat
-var hp;
+var announcer = $("#announcer");
+var characters = $("#characters");
+var player = $("#player");
+var fighting = $("#fighting");
+var enemies = $("#enemies")
 
-select();
+var turn = 0;
 
-console.log("select");
+var plr;
+var ene;
 
-function select(){
-    announcer.text("choose your character")
+var playerHp ;
+var opHp ;
+
+var p = false;
+var op= false;
+
+var dead = false;
+var specmeter;
+
+
+var obiwan={
+    atk: 10,
+    hp: 100,
+    adratio: 50,
+    special: function(){
+
+    }
+}
+
+var sid = {
+    atk : 8,
+    hp :80,
+    adratio:20,
+    special: function(){
+
+    }
+}
+
+var darth = {
+    atk : 12,
+    hp : 120,
+    adratio: 70,
+    special: function(){
+
+    }
+}
+
+var luke = {
+    atk: 90,
+    hp: 90,
+    adratio: 40,
+    special: function(){
+
+    }
+}
+
+$("#atkbtn").on("click", function(){
+    attack(ene);
+})
+
+card.on("click", function(){
+    // player character select
+
+    if(p==false){
+        $(this).addClass("player");
+        player.append(this);
+        p=true;
+        plr = this;
+        enemies.append(characters.contents());
+        return;
+    }
+
+    // enemy select
+
+    if(op==false && $(this).hasClass("player")==false){
+        $(this).addClass("enemy");
+        fighting.append(this);
+        op=true;
+        ene = this;
+        dead = false;
+        return;
+    }
+
+    // if(op == true && $(this).hasClass("enemy") == true){
+        
+    //     attack(this);
+        
+    //     if(dead == true){
+    //         deathrattle(this);
+            
+
+            
+    //         op = false;
+    //         return;
+    //     }
+    //     return;
+    // }
+})
+
+
+$(".btn").on("click", function(){
+    specmeter++;
+})
+function attack(enemy){
     
-    $("#characters").children().on("click", function(){
-        console.log("poop")
-        start(this);
+    
 
-    })
+    console.log(plr)
+    switch (plr.id){
+        case "obiwan":
+            opHp = parseInt($(enemy).children("div.card-body").children("div.row.hp").children("h4.points").text());
+            opHp -=  obiwan.atk
+            $(enemy).children("div.card-body").children("div.row.hp").children("h4.points").text(opHp)
+            if(opHp <= 0){
+                dead = true
+            }
+            break;
+        case "sid":
+            opHp = parseInt($(enemy).children("div.card-body").children("div.row.hp").children("h4.points").text());
+            opHp -=  sid.atk
+            $(enemy).children("div.card-body").children("div.row.hp").children("h4.points").text(opHp)
+            if(opHp <= 0){
+                dead = true
+            }
+            break;
+        case "darth":
+            opHp = parseInt($(enemy).children("div.card-body").children("div.row.hp").children("h4.points").text());
+            opHp -=  darth.atk
+            $(enemy).children("div.card-body").children("div.row.hp").children("h4.points").text(opHp)
+            if(opHp <= 0){
+                dead = true
+            }
+            break;
+        case "luke":
+            opHp = parseInt($(enemy).children("div.card-body").children("div.row.hp").children("h4.points").text());
+            opHp -=  luke.atk
+            $(enemy).children("div.card-body").children("div.row.hp").children("h4.points").text(opHp)
+            if(opHp <= 0){
+                dead = true
+            }
+            break;
+    }
+}
+function defend(){
+    
+}
+function special(){
+
+}
+
+function deathrattle(guy){
+    var death = document.createElement('audio');
+
+    if(guy.id=="vader"){
+        death.setAttribute('src', 'assets/sound/nooo.mp3');
+        death.play();
+        fighting.empty();
+    }
+    if(guy.id == "sid"){
+        death.setAttribute('src','assets/sound/palpatineDeath.mp3')
+        death.play()
+        $(death).on("ended", function(){
+            var kill = confirm("Are you sure you want to kill palpatine?")
+            if(kill == true){
+                death.setAttribute("src","assets/sound/realDeath.mp3" )
+                fighting.empty();
+            }else{
+                special("sid");
+            }
+
+        })
+    }
     
 }
 
-function start(selected){  
+// card.on("click",".enemy" function(){
+
+// })
+
+
+// function select(){
+
+//         if(p==false){
+            
+//             card.on("click", function(){      
+                
+//                 if($(this).hasClass("enemy") == false){
+//                     $(this).addClass("player");
+//                     if(player.children().length == 0){
+//                         player.append(this);
+//                         p=true;
+                        
+//                     }
+//                 }
+
+                
+//             })
+
+//            for(i in characters.contents().length){
+//                 console.log(this)
+//             }
+//             start();
+//             console.log("end")     
+            
+//         }
+// }
+
+
+
+function start(){
+    select();
+
     
-    console.log("start")
-    
-    $("#player").append(selected);
-
-    $("#enemies").append($("#characters").contents());
-
-    $("#announcer").text("Pick an enemy to fight");
-    
-    $("#enemies").children().on("click", function(){
-        $("#fighting").append(this);
-        console.log("fighting enemey" + this );
-        fight(this);
-    })
-
-    
-}  
-    
-    
-    
 
 
+    // 
+    // enemies.append(characters.contents())
+    // announcer.text("choose an opponent")
+    // console.log(playerHp);
+    // if(fighting.children().length == 0){
+        
+        
+// }
 
+// function fight(op){
 
-function fight(op){
-    
-    // $("#announcer").text("Your are fighting" + $opid.
-    console.log("fight")
-    $(op).on("click", function(){
-        attack(this)
-        $(op).children("div.card-body").children("div.row.hp").children("h4.points").text(hp)
-    })
-
-}
-
-function attack(target){
-    hp = parseInt($(target).children("div.card-body").children("div.row.hp").children("h4.points").text());
-
-    hp = hp - 10;
-    console.log(hp)
-    return hp;
-    // $(target).children("div.card-body").children("div.row.hp").children("h4.points").text(hp);
-    
 }
